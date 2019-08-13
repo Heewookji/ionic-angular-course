81d2c14b975d893ec263f7c2f52596192baa2db7
heewookji <wlgmldnr@daum.net>
2019-08-13 19:23:55 +0900

    ionic-component section finished

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const cancelBtn = document.querySelector('#btn-cancel');
 const confirmBtn = document.querySelector('#btn-confirm');
 const expensesList = document.querySelector('#expenses-list');
 const expensesTotal = document.querySelector('#expenses-total');
+const alertCtrl = document.querySelector('ion-alert-controller');
 
 
 const clear = () => {
@@ -19,22 +20,29 @@ confirmBtn.addEventListener('click', () => {
     const enteredReason = reasonInput.value;
     const enteredAmount = amountInput.value;
 
-    if(enteredReason.trim().length <= 0 || enteredAmount <= 0 || 
-    enteredAmount.trim().length <= 0){
+    if (enteredReason.trim().length <= 0 || enteredAmount <= 0 ||
+        enteredAmount.trim().length <= 0) {
+        alertCtrl.create({
+            message: 'Please enter valid reason and amount!',
+            header: 'Invalid Input',
+            buttons: ['Okay']
+        }).then(alertElement => {
+            alertElement.present();
+        });
         return;
     }
 
-    total += +enteredAmount; 
+    total += +enteredAmount;
 
     const newItem = document.createElement('ion-item');
     newItem.textContent = enteredReason + ': $' + enteredAmount;
 
-    expensesList.appendChild(newItem); 
+    expensesList.appendChild(newItem);
 
     expensesTotal.textContent = '';
-    expensesTotal.textContent = '$'+ total;
+    expensesTotal.textContent = '$' + total;
 
     clear();
-     
+
 })
 
