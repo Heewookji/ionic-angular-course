b149f9d75514436ab0219ff8b61d4d5774a1a89a
heewookji <wlgmldnr@daum.net>
2019-10-19 23:43:55 +0900

    map modal present and module error

--- src/app/shared/map-modal/map-modal.component.ts
+++ src/app/shared/map-modal/map-modal.component.ts
@@ -1,14 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ModalController } from '@ionic/angular';
 
 @Component({
-  selector: 'app-map-modal',
-  templateUrl: './map-modal.component.html',
-  styleUrls: ['./map-modal.component.scss'],
+  selector: "app-map-modal",
+  templateUrl: "./map-modal.component.html",
+  styleUrls: ["./map-modal.component.scss"]
 })
 export class MapModalComponent implements OnInit {
 
-  constructor() { }
+  constructor(private modalCtrl: ModalController) {}
 
   ngOnInit() {}
 
+  onCancel() {
+    this.modalCtrl.dismiss();
+  }
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,13 +1,15 @@
 
 
 import { NgModule } from '@angular/core';
-import { MapModalComponent } from './map-modal/map-modal.component';
 import { LocationPickerComponent } from './pickers/location-picker/location-picker.component';
+import { MapModalComponent } from './map-modal/map-modal.component';
 import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+
 @NgModule({
     declarations: [LocationPickerComponent, MapModalComponent],
+    imports: [CommonModule, IonicModule],
     exports: [LocationPickerComponent, MapModalComponent],
-    imports: [CommonModule],
     entryComponents: [MapModalComponent]
 })
 export class SharedModule {
